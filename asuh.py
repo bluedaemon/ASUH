abd13f4bf99991c3f165e5d4c4681acb511c30ac
Patrick Gray <patgray1127@gmail.com>
2013-03-26 22:58:52 -0400

    general updates to comments and output

--- asuh.py
+++ asuh.py
@@ -1,6 +1,10 @@
+# Regular Expression Library for parsing
 import re
+#Paramiko for SSH
 import paramiko
 from paramiko import SSHClient
+
+#Strings as functions
 
 ASUH_rand='''
 def ASUH_rand(N,M):
@@ -11,7 +15,7 @@ def ASUH_rand(N,M):
 ASUH_min='''
 import random, Queue, threading
 def ASUH_min(m):
-	print m
+#	print m
 	q = Queue.PriorityQueue()
 	for i in range(0,len(m)):
 		tz = threading.Thread(target=a_min,args=(m[i],q))
@@ -23,43 +27,57 @@ def a_min(a,q):
 
 '''
 
-#variable initialization
+#Variable initialization that store variable and data from text file
 variable_array=[]
-function_array=[]
+workflow_array=[]
 parameter_array=[]
 ASUHfuncs=["rand","min"]
-temp=[]
+function_array=[]
+output = open('output.txt','w')
 
-#parse input file
+#Parse input file using regular expressions
 f = open("b.txt")
 for line in f:
 	parameter_array.append(re.search('\((.*?)\)',line).group(1))
 	variable_array.append(re.search('(.*?)\=',line).group(1))
-	function_array.append(re.search('\=(.*?)\(',line).group(1))
+	workflow_array.append(re.search('\=(.*?)\(',line).group(1))
 
-#execute valid ASUH functions
-for i in xrange(len(function_array)):
-	if function_array[i] in ASUHfuncs:
-		temp.append((variable_array[i]+'='+'ASUH_'+function_array[i]+'('+parameter_array[i]+')'))
-user_ = 'jl'
-pass_ = 'xxxx'
+#Conversion of workflow functions into server computations
+for i in xrange(len(workflow_array)):
+	if workflow_array[i] in ASUHfuncs:
+		function_array.append((variable_array[i]+'='+'ASUH_'+workflow_array[i]+'('+parameter_array[i]+')'))
 
+#Insert your username and password here
+#We recommend using password-less login via SSH Keys in order to preserve the security of passwords
+user_ = 'pg'
+pass_ = ''
+
+#SSH Client initialization
 ssh_client = SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect('twentyseven.info',username=user_, password=pass_)
 chan = ssh_client.invoke_shell()
 stdin = chan.makefile('wb')
 stdout = chan.makefile('rb')
+
+#SSH Client code execution
 stdin.write(
+
 '''
 python
 '''
+
 +ASUH_rand+ASUH_min)
-for i in temp:
+
+for i in function_array:
 	stdin.write('\n'+i)
+
 stdin.write('''
 exit()
 exit
 ''')
-print stdout.read()
+
+output.write(stdout.read())
+
+#SSH Client code termination
 ssh_client.close()
