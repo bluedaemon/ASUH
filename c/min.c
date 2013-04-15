0e0d61501a347e46c1aa5413c3f74fe2fa90a8b2
Patrick Gray <patgray1127@gmail.com>
2013-04-15 00:42:34 -0400

    dumb min function fleshed out

--- c/min.c
+++ c/min.c
@@ -1,13 +1,32 @@
 /* This is the sequential min function for ASUH */
-
+/*
+	This function takes the number of "vectors" and number of "places" and a filename
+	example : ./min 10 10 input.csv output.csv
+*/
 #include "fileio.h"
 int main(int argc, char **argv)
 {
-	int i,j;
-	int r,c;
-	int **minArray = createArray(r, c);
-
-
-	freeArray(r, c, minArray);
+	int i, j;
+	int vector, place;
+	vector = atoi(argv[1]);
+	place = atoi(argv[2]);
+	int **minArray = createArray(vector, place);
+	readFile(vector, place, minArray, argv[3]);
+	int min = minArray[0][0];
+	for(j=0;j < vector; j++)
+	for(i=0; i < place; i++)
+	{
+		printf("%i ", minArray[j][i]);
+		if(minArray[j][i] < min)
+			min = minArray[j][i];
+	}
+	int **answer;
+	answer = (int **)malloc(sizeof(int *));
+	*answer = (int*)malloc(sizeof(int));
+	**answer = min;
+	writeFile(1, 1, answer, argv[4]);
+	free(*answer);
+	free(answer);
+	freeArray(vector, place, minArray);
 	return 0;
 }
