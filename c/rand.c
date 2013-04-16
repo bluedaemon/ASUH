ee183c59a28c481d8d58aba5ed8a5edbc2798fe0
Patrick Gray <patgray1127@gmail.com>
2013-04-16 18:18:21 -0400

    added rand.c

---
+++ c/rand.c
@@ -0,0 +1,21 @@
+#include "fileio.h"
+
+int main(int argc, char **argv)
+{
+	if(argc < 4)
+	{
+		perror("rand.c: not enough arguments");
+		exit(1);
+	}
+	int vector = atoi(argv[1]);
+	int place = atoi(argv[2]);
+	int **randArray = createArray(vector, place);
+
+	int i,j;
+	for(j=0;j < vector; j++)
+	for(i=0;i < place; i++)
+		randArray[j][i] = random() / (RAND_MAX / 10);
+	writeFile(vector, place, randArray, argv[3]);
+	freeArray(vector, place, randArray);
+	return 0;
+}
