3bb16188dd537666d3c10373eb7b06c4f4c328f3
Jae Lee <jlee734@gmail.com>
2013-04-26 23:11:01 -0400

    Finished, just needs testing when cuda is done

--- asuh_load.py
+++ asuh_load.py
@@ -1,108 +1,138 @@
-#Regular Expression Library for parsing
 import re
-#Paramiko for SSH
+import os
+import getpass
 import paramiko
-import time
 from paramiko import SSHClient
 
-#Strings as functions
+#TODO RENAME ALL C FILE TO HAVE C_ AS PREFIX AND CUDA FILE TO HAVE CU_ AS PREFIX (c_max.c cu_max.cu)
 
-ASUH_rand='''
-def ASUH_rand(N,M):
-	return [[random.randrange(1,100) for x in range(N)] for y in range(M)]
-
-'''
-
-ASUH_min='''
-import random, Queue, threading
-def ASUH_min(m):
-#	print m
-	q = Queue.PriorityQueue()
-	for i in range(0,len(m)):
-		tz = threading.Thread(target=a_min,args=(m[i],q))
-		tz.start()
-	print q.get()
-
-def a_min(a,q):
-	q.put(min(a))
-
-'''
-
-#Variable initialization that store variable and data from text file
-variable_array=[]
-workflow_array=[]
-parameter_array=[]
-ASUHfuncs=["rand","min"]
-function_array=[]
-output = open('output.txt','w')
-
-#Parse input file using regular expressions
-f = open("b.txt")
-for line in f:
-	parameter_array.append(re.search('\((.*?)\)',line).group(1))
-	variable_array.append(re.search('(.*?)\=',line).group(1))
-	workflow_array.append(re.search('\=(.*?)\(',line).group(1))
-
-#Conversion of workflow functions into server computations
-for i in xrange(len(workflow_array)):
-	if workflow_array[i] in ASUHfuncs:
-		function_array.append((variable_array[i]+'='+'ASUH_'+workflow_array[i]+'('+parameter_array[i]+')'))
+output = open('output.txt', 'w')
 
 #Insert your username and password here
 #We recommend using password-less login via SSH Keys in order to preserve the security of passwords
 user_ = 'outcode'
-pass_ =''
-host= 'mphase.rutgers.edu'
-port= 22
+pass_ ='127001402'
+host_= 'mphase.rutgers.edu'
+port_= 22
+
 #SSH Client initialization
+hostkeytype = None
+hostkey = None
+try:
+    host_keys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+except IOError:
+    try:
+        # try ~/ssh/ too, because windows can't have a folder named ~/.ssh/
+        host_keys = paramiko.util.load_host_keys(os.path.expanduser('~/ssh/known_hosts'))
+    except IOError:
+        print '*** Unable to open host keys file'
+        host_keys = {}
+
+if host_keys.has_key(host_):
+    hostkeytype = host_keys[host_].keys()[0]
+    hostkey = host_keys[host_][hostkeytype]
+    print 'Using host key of type %s' % hostkeytype
+
 ssh_client = SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(host,username=user_, password=pass_)
-transport = paramiko.Transport((host,port))
-transport.connect(username=user_,password=pass_)
-sftp = paramiko.SFTPClient.from_transport(transport)
+ssh_client.connect(hostname=host_, username=user_, password=pass_)
 
-#download
-#filepath = ''
-#localpath= ''
-#sftp.get(filepath,localpath)
+t = paramiko.Transport((host_,port_))
+t.connect(username=user_,password=pass_,hostkey=hostkey)
+sftp = paramiko.SFTPClient.from_transport(t)
 
-#upload
-#filepath = ''
-#localpath= ''
-#sftp.put(filepath,localpath)
+
+#Variable initialization that store variable and data from text file
+workflow_array=[]
+parameter_array=[]
+commands=["cd\n"]
+cuda_fc=[]
+c_fc=[]
+filepaths = ['c/fileio.c', 'c/fileio.h']
+output = open('output.txt','w')
+
+#Parse input file using regular expressions
+f = open("example.txt")
+for line in f:
+	parameter_array.append(re.search('\((.*?)\)',line).group(1))
+	function = re.search('(.*?)\(',line).group(1)
+	workflow_array.append(function)
+	filepaths.append("c/"+function+".c")
+	filepaths.append("cuda/"+function+".cu")
+
+for i in range(0,len(workflow_array)):
+	commands.append("gcc -o c_"+workflow_array[i]+" c/fileio.h c/fileio.c "+"c/"+workflow_array[i]+".c\n")
+	commands.append("nvcc -o cu_"+workflow_array[i]+" c/fileio.h c/fileio.c "+"cuda/"+workflow_array[i]+".cu\n")
+	c_run="./c_"+workflow_array[i]+" "
+	cu_run="./cu_"+workflow_array[i]+" "
+	#Parameter Parsing
+	for prm in parameter_array[i].split(','):
+		c_run=c_run+prm+" "
+		cu_run=cu_run+prm+" "
+	#Add to respective list (c or cuda)
+	c_fc.append(c_run+"\n")
+	cuda_fc.append(cu_run+"\n")
+
+
+dirlist = sftp.listdir('.')
+print "DirList:", dirlist
+if not 'c' in dirlist:
+	sftp.mkdir("c")
+if not 'cuda' in dirlist:
+	sftp.mkdir("cuda")
+for i in filepaths:
+	sftp.put(i,i)
 
 chan = ssh_client.invoke_shell()
+chan.send('rsh GpuNode01\n')
+
 stdin = chan.makefile('wb')
 stdout = chan.makefile('rb')
 
-#SSH Client code execution
+buff = ''
+while not buff.endswith('$ '):
+    resp = chan.recv(9999)
+    buff += resp
+
+#Run on GPU First
+commands.append(cuda_fc[0]+" &\n")
+
+#Load Balance
+for cnt in range(1,len(c_fc)):
+	commands=commands+["""#!/bin/sh\n""",
+          		   """cmd='top -b -n 2 | grep "Cpu(s)" | tail -1 | grep -o -P ".{0,5}%id" | sed "s/.\{3\}$//"'\n""",
+		 	   """cpu_load=$(eval $cmd | bc)\n""",
+		 	   """cmd='nvidia-smi -a | grep -m 1 "Gpu" | sed "s/[^0-9]*//g"'\n""",
+          	  	   """gpu_load=$(eval $cmd | bc)\n""",
+		  	   "if [[ $gpu_load < $cpu_load ]]\nthen\n"+c_fc[cnt]+" &\nelse\n"+cuda_fc[cnt]+" &\nfi\n",
+		  	   """exit\n"""
+         ]
+
+for line in commands:
+	buff = ''
+	print line
+	chan.send(line)
+
+	while not buff.endswith('$ '):
+		resp = chan.recv(9999)
+		buff += resp
+
+	print 'buff', buff
+
 stdin.write(
-#calc=$(eval $load | bc)
 '''
-#!/bin/sh 
-cmd="top -b -n 2 | grep 'Cpu(s)' | tail -1 | grep -o -P '.{0,5}%id' | sed 's/.\{3\}$//'"
-load=$(eval $cmd | bc)
-echo $load
-if [[ $load > 40 ]]
-then
-openssl enc -aes-256-cbc -salt -in file.txt -out file.enc -pass pass:MySillyPassword
-else
-echo "Running on the GPU"
-fi
 exit
 ''')
 
-#for i in function_array:
-	#stdin.write('\n'+i)
 
-#stdin.write('''
-
-#''')
+#openssl enc -aes-256-cbc -salt -in file.txt -out file.enc -pass pass:MySillyPassword
 
 output.write(stdout.read())
 
 #SSH Client code termination
 ssh_client.close()
 sftp.close()
-
+#sftp.get('o1.csv','o1.csv')
+#sftp.get('o2.csv','o2.csv')
+#sftp.get('cpu.info','cpu.info')
+t.close()
