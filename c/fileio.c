5c855830ca711f1d15a071066b512f1f859f24a9
Andrew Conegliano <aconegliano27@gmail.com>
2013-04-27 00:59:02 -0400

    cleanup and fixed fileio for multiple digit numbers

--- c/fileio.c
+++ c/fileio.c
@@ -4,6 +4,7 @@
 int readFile(int vector, int place, int **data, char *filename)
 {
 	FILE *inputfile;
+	char *temp=(char*)malloc(sizeof(char)*4294967295);
 	if((inputfile = fopen(filename, "r")) == NULL)
 	{
 		perror("fileio.h read");
@@ -11,7 +12,7 @@ int readFile(int vector, int place, int **data, char *filename)
 		exit(1);
 	}
 
-	int i, j, hold;
+	int i, j, hold,index=0;
 
 	for(j = 0; j < vector; j++)
 	for(i = 0; i < place; i++)
@@ -22,19 +23,118 @@ int readFile(int vector, int place, int **data, char *filename)
 			hold = getc(inputfile);
 			if(hold == '-')
 			{
-				hold = getc(inputfile);
-				data[j][i] = -1 * (hold - 48);
+				index=0;
+				temp[index] = getc(inputfile);
+				while(temp[index] != '\n' && temp[index] != ','){
+					index++;
+					temp[index] = getc(inputfile);
+				}
+				temp[index]='\0';
+				data[j][i] = atoi(temp);
 			}
-			else
-				data[j][i] = hold - 48;
+			else{
+				index=0;
+				temp[index]=hold;
+				while(temp[index] != '\n' && temp[index] != ','){
+					index++;
+					temp[index] = getc(inputfile);					
+				}
+				temp[index]='\0';
+				data[j][i] = atoi(temp);
+			}
 		}
 		else if(hold == '-')
 		{
-			hold = getc(inputfile);
-			data[j][i] = -1 * (hold - 48);
+			index=0;
+			temp[index] = hold;
+			while(temp[index] != '\n' && temp[index] != ','){
+				index++;
+				temp[index] = getc(inputfile);
+			}
+			temp[index]='\0';
+			data[j][i] = atoi(temp);
+			}
+		else{
+			index=0;
+			temp[index]=hold;
+			while(temp[index] != '\n' && temp[index] != ','){
+				index++;
+				temp[index] = getc(inputfile);
+				
+			}
+			temp[index]='\0';
+			data[j][i] = atoi(temp);
 		}
-		else
-			data[j][i] = hold - 48;
+	}
+
+	fclose(inputfile);
+	return 0;
+}
+
+int readFileCUDA(int vector, int place, int *data, char *filename)
+{
+	FILE *inputfile;
+	char *temp=(char*)malloc(sizeof(char)*4294967295);
+	if((inputfile = fopen(filename, "r")) == NULL)
+	{
+		perror("fileio.h read");
+		fprintf(stderr, "File does not exist");
+		exit(1);
+	}
+
+	int i, j, hold,index=0;
+
+	for(j = 0; j < vector; j++)
+	for(i = 0; i < place; i++)
+	{
+		hold = getc(inputfile);
+		if(hold == '\n' || hold == ',')
+		{
+			hold = getc(inputfile);
+			if(hold == '-')
+			{
+				index=0;
+				temp[index] = getc(inputfile);
+				while(temp[index] != '\n' && temp[index] != ','){
+					index++;
+					temp[index] = getc(inputfile);
+				}
+				temp[index]='\0';
+				data[j*place+i] = atoi(temp);
+			}
+			else{
+				index=0;
+				temp[index]=hold;
+				while(temp[index] != '\n' && temp[index] != ','){
+					index++;
+					temp[index] = getc(inputfile);					
+				}
+				temp[index]='\0';
+				data[j*place+i] = atoi(temp);
+			}
+		}
+		else if(hold == '-')
+		{
+			index=0;
+			temp[index] = hold;
+			while(temp[index] != '\n' && temp[index] != ','){
+				index++;
+				temp[index] = getc(inputfile);
+			}
+			temp[index]='\0';
+			data[j*place+i] = atoi(temp);
+			}
+		else{
+			index=0;
+			temp[index]=hold;
+			while(temp[index] != '\n' && temp[index] != ','){
+				index++;
+				temp[index] = getc(inputfile);
+				
+			}
+			temp[index]='\0';
+			data[j*place+i] = atoi(temp);
+		}
 	}
 
 	fclose(inputfile);
